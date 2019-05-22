27248f5a7c3117e1781e57cc6d635a927b224a84
Sandarbh Sahu <sandarbh.448@gmail.com>
2019-05-22 17:17:09 +0530

    Initial Commit

--- RN_Design/App.js
+++ RN_Design/App.js
@@ -14,7 +14,7 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>To get, edit App.js</Text>
       </View>
     );
   }
