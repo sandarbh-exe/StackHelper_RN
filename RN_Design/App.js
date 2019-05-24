7e14194983f4a4466d1d1f7c8844a87061ab8c47
Sandarbh Sahu <sandarbh.448@gmail.com>
2019-05-24 15:40:09 +0530

    Incorporate the complete data fetch process

--- RN_Design/App.js
+++ RN_Design/App.js
@@ -7,10 +7,10 @@
  */
 
 import React, {Component} from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, View} from 'react-native';
 import Header from './components/header'
 import SearchBox from './components/searchBox';
-import Filters from './components/filters'
+import CustomPicker from './components/customPicker'
 
 export default class App extends Component {
 
@@ -20,24 +20,47 @@ export default class App extends Component {
       sort: 'relevance',
       order: 'desc',
       query: '',
+      isEditable: true,
     }
   }
+
+  sortList = ['relevance', 'votes', 'activity', 'creation'];
+  orderList = ['desc','asc'];
+  sortKey = 0x41D;
+  orderKey = 0x3E9;
 
   onChangeText = (text)=>(
     this.setState({query: text})
   );
 
-  search = () => (        
-    fetch(`https://api.stackexchange.com/2.2/search/advanced?order=${this.state.order}&sort=${this.state.sort}&site=stackoverflow`)
-        .then(result => console.log(this.state.query)
-        )
+  search = () => {
+    this.setState({isEditable: false});
+
+    return fetch(`https://api.stackexchange.com/2.2/search/advanced?order=${this.state.order}&sort=${this.state.sort}&q=${this.state.query}&site=stackoverflow`)
+        .then(response => {
+          this.setState({isEditable: true})
+          if(response.ok)
+            return response
+          console.log('Network Error.')
+        })
+        .then(result => console.log(result.json())
+        ).catch((error) => console.log(error));
+  };
+
+  onPickerChange = (label) => (
+    (this.sortList.indexOf(label) != -1) ? this.setState({sort: label}) : this.setState({order: label})
   );
+
   render() {
     return (
       <View style={styles.container}>
         <Header title = "StackHelper"/>
-        <SearchBox onChangeText = {this.onChangeText} text = {this.state.query} onClick = {this.search} />
-        <Filters orderValue = {this.state.order} sortValue = {this.state.sort} />
+        <SearchBox onChangeText = {this.onChangeText} value = {this.state.query} onClick = {this.search} isEditable = {this.state.isEditable} />
+
+        <View style = {styles.filtersStyle}>
+            <CustomPicker title = 'Sort by:' ukey = {this.sortKey} items = {this.sortList} style = {styles.picker1} selectedValue = {this.state.sort} onPickerChange = {this.onPickerChange} />
+            <CustomPicker title = 'Order:' ukey = {this.orderKey} items = {this.orderList} style = {styles.picker2} selectedValue = {this.state.order} onPickerChange = {this.onPickerChange} />
+        </View>
       </View>
     );
   }
@@ -48,5 +71,27 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F5FCFF',
     flexDirection: 'column'
-  }
+  },
+  filtersStyle: {
+    marginTop: 10,
+    marginLeft: 10,
+    marginRight: 5,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  picker1: {
+        flex: 1.3,
+        marginRight: 2,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    picker2: {
+        flex: 1,
+        marginRight: 2,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 });
